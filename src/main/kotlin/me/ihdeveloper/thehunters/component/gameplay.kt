51b81de20418a9c9e48409cacd1267ce76583fb6
iHDeveloper <20463031+iHDeveloper@users.noreply.github.com>
2020-06-22 14:17:16 +0300

    Create the target component and set its max health to 30

---
+++ src/main/kotlin/me/ihdeveloper/thehunters/component/gameplay.kt
@@ -0,0 +1,48 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 iHDeveloper ( Hamza Aljuaid )
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package me.ihdeveloper.thehunters.component
+
+import me.ihdeveloper.thehunters.GameComponentOf
+import me.ihdeveloper.thehunters.GamePlayer
+
+const val TYPE_GAMEPLAY_TARGET: Short = 301
+
+class TargetComponent (
+        override val gameObject: GamePlayer
+) : GameComponentOf<GamePlayer>() {
+
+    override val type = TYPE_GAMEPLAY_TARGET
+
+    override fun onInit(gameObject: GamePlayer) {
+        val health = 20.0 * 3
+        gameObject.entity.maxHealth = health
+        gameObject.entity.health = health
+    }
+
+    override fun onDestroy(gameObject: GamePlayer) {
+    }
+
+}
