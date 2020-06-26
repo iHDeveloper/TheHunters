9fb550c62ae2edea1bc8f0ea529bcfd2e3f958f9
iHDeveloper <20463031+iHDeveloper@users.noreply.github.com>
2020-06-26 10:07:40 +0300

    Create component for the side that lost in the end of the game

--- src/main/kotlin/me/ihdeveloper/thehunters/component/end.kt
+++ src/main/kotlin/me/ihdeveloper/thehunters/component/end.kt
@@ -29,9 +29,11 @@ import me.ihdeveloper.thehunters.GameComponentOf
 import me.ihdeveloper.thehunters.GamePlayer
 import me.ihdeveloper.thehunters.util.COLOR_BOLD
 import me.ihdeveloper.thehunters.util.COLOR_GREEN
+import me.ihdeveloper.thehunters.util.COLOR_RED
 import me.ihdeveloper.thehunters.util.COLOR_YELLOW
 
 const val TYPE_END_VICTORY: Short = 901
+const val TYPE_END_DEFEATED: Short = 902
 
 class VictoryComponent (
         val target: Boolean,
@@ -51,6 +53,32 @@ class VictoryComponent (
                     append("You proved that you can't be hunted!")
                 else
                     append("Good hunt!")
+            }.toString())
+            time(10, 60, 10)
+        }
+    }
+
+    override fun onDestroy(gameObject: GamePlayer) {}
+}
+
+class DefeatedComponent(
+        val target: Boolean,
+        override val gameObject: GamePlayer
+) : GameComponentOf<GamePlayer>() {
+
+    override val type = TYPE_END_DEFEATED
+
+    override fun onInit(gameObject: GamePlayer) {
+        gameObject.get<TitleComponent>(TYPE_TITLE).run {
+            reset()
+            title("$COLOR_RED${COLOR_BOLD}You lost!")
+            subtitle(StringBuilder().apply {
+                append("$COLOR_YELLOW")
+                append("$COLOR_BOLD")
+                if (target)
+                    append("You can be hunted!")
+                else
+                    append("Better luck! next time.")
             }.toString())
             time(10, 60, 10)
         }
