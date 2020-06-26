5039f94918ca29a0fb2bb0a7360a5cc9dd89bda9
iHDeveloper <20463031+iHDeveloper@users.noreply.github.com>
2020-06-26 11:18:54 +0300

    Move: GameDeathComponent to the gameplay/common file

--- src/main/kotlin/me/ihdeveloper/thehunters/gameplay.kt
+++ src/main/kotlin/me/ihdeveloper/thehunters/gameplay.kt
@@ -29,7 +29,6 @@ import me.ihdeveloper.thehunters.command.GameShoutCommand
 import me.ihdeveloper.thehunters.command.GameSetSpawnCommand
 import me.ihdeveloper.thehunters.component.ConfigurationComponent
 import me.ihdeveloper.thehunters.component.CountdownComponent
-import me.ihdeveloper.thehunters.component.DeathComponent
 import me.ihdeveloper.thehunters.component.NoDamageComponent
 import me.ihdeveloper.thehunters.component.ScoreboardComponent
 import me.ihdeveloper.thehunters.component.TYPE_COUNTDOWN
@@ -59,27 +58,12 @@ import me.ihdeveloper.thehunters.event.player.GameJoinEvent
 import me.ihdeveloper.thehunters.event.player.GameQuitEvent
 import me.ihdeveloper.thehunters.event.target.TargetJoinEvent
 import me.ihdeveloper.thehunters.event.target.TargetQuitEvent
-import me.ihdeveloper.thehunters.util.COLOR_CYAN
-import me.ihdeveloper.thehunters.util.COLOR_DARK_PURPLE
-import me.ihdeveloper.thehunters.util.COLOR_GOLD
-import me.ihdeveloper.thehunters.util.COLOR_GRAY
-import me.ihdeveloper.thehunters.util.COLOR_GREEN
-import me.ihdeveloper.thehunters.util.COLOR_PURPLE
-import me.ihdeveloper.thehunters.util.COLOR_RED
-import me.ihdeveloper.thehunters.util.COLOR_WHITE
-import me.ihdeveloper.thehunters.util.COLOR_YELLOW
 import me.ihdeveloper.thehunters.util.COUNTDOWN_GAMEPLAY_GET_READY
 import me.ihdeveloper.thehunters.util.COUNTDOWN_GAMEPLAY_INTRO
 import org.bukkit.Bukkit
 import org.bukkit.Location
-import org.bukkit.block.Block
-import org.bukkit.entity.Entity
-import org.bukkit.entity.EntityType
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.entity.EntityDamageByBlockEvent
-import org.bukkit.event.entity.EntityDamageByEntityEvent
-import org.bukkit.event.entity.EntityDamageEvent
 import java.util.UUID
 import kotlin.random.Random
 
@@ -245,207 +229,4 @@ class Gameplay : GameObject(
         countdown.stop()
     }
 
-}
-
-abstract class GameDeathComponent : DeathComponent() {
-
-    abstract override val gameObject: GamePlayer
-
-    abstract override val type: Short
-
-    protected abstract val name: String
-
-    override fun byEntity(killer: Entity, event: EntityDamageByEntityEvent) {
-        broadcast {
-            if (killer.type === EntityType.FIREBALL) {
-                append("$COLOR_YELLOW was fire balled!")
-                return
-            }
-
-            append("$COLOR_YELLOW was killed by")
-
-            append(when(killer.type) {
-                EntityType.ARROW -> "$COLOR_RED Arrow"
-                EntityType.BLAZE -> "$COLOR_GOLD Blaze"
-                EntityType.CAVE_SPIDER -> "$COLOR_DARK_PURPLE Cave Spider"
-                EntityType.ENDER_DRAGON -> "$COLOR_DARK_PURPLE Ender Dragon"
-                EntityType.ENDERMAN -> "$COLOR_DARK_PURPLE Enderman"
-                EntityType.FALLING_BLOCK -> "$COLOR_CYAN Falling Block"
-                EntityType.GHAST -> "$COLOR_GRAY Ghost"
-                EntityType.GUARDIAN -> "$COLOR_CYAN Guardian"
-                EntityType.IRON_GOLEM -> "$COLOR_WHITE Iron Golem"
-                EntityType.MAGMA_CUBE -> "$COLOR_RED Magma Cube"
-                EntityType.SILVERFISH -> "$COLOR_GRAY Silverfish"
-                EntityType.SKELETON -> "$COLOR_GRAY Skeleton"
-                EntityType.SPIDER -> "$COLOR_PURPLE Spider"
-                EntityType.PIG_ZOMBIE -> "$COLOR_PURPLE Pig Zombie"
-                EntityType.WITHER_SKULL -> "$COLOR_RED Wither Skull"
-                EntityType.WOLF -> "$COLOR_GRAY Wolf"
-                EntityType.ZOMBIE -> "$COLOR_GREEN Zombie"
-                else -> "$COLOR_GRAY Unknown"
-            })
-        }
-    }
-
-    override fun byEntityExplosion(entity: Entity) {
-        broadcast {
-            append("$COLOR_YELLOW blew up")
-
-            append(when (entity.type) {
-                EntityType.CREEPER -> " by$COLOR_GREEN Creeper"
-                EntityType.PRIMED_TNT -> " by$COLOR_RED TNT"
-                EntityType.MINECART_TNT -> " by$COLOR_RED Minecart$COLOR_YELLOW with$COLOR_RED TNT"
-                else -> "!"
-            })
-        }
-    }
-
-    override fun byBlock(killer: Block, event: EntityDamageByBlockEvent) {
-        broadcast {
-            append("$COLOR_YELLOW was killed by a block aka")
-
-            // FIX ME: Give another name instead of the enum name
-            append("$COLOR_RED ${killer.type.name}")
-        }
-    }
-
-    override fun byBlockExplosion(killer: Block) {
-        broadcast {
-            append("$COLOR_YELLOW blew up")
-        }
-    }
-
-    override fun byContact() {
-        // FIX ME: This death reason should be improved and checked if it can happen at all
-
-        broadcast {
-            append("$COLOR_YELLOW contacted the wrong entity")
-        }
-    }
-
-    override fun byDrowning() {
-        broadcast {
-            append("$COLOR_YELLOW drowned")
-        }
-    }
-
-    override fun byFalling() {
-        broadcast {
-            append("$COLOR_YELLOW fell from high place")
-        }
-    }
-
-    override fun byFire() {
-        broadcast {
-            append("$COLOR_YELLOW fired away")
-        }
-    }
-
-    override fun byLava() {
-        broadcast {
-            append("$COLOR_YELLOW tried to swim in")
-            append("$COLOR_RED Lava")
-        }
-    }
-
-    override fun byLighting() {
-        broadcast {
-            append("$COLOR_YELLOW lighted away")
-        }
-    }
-
-    override fun byMagic() {
-        broadcast {
-            append("$COLOR_YELLOW was killed by")
-            append("$COLOR_PURPLE Magic")
-        }
-    }
-
-    override fun byMelting() {
-        // FIX ME: This death reason should be improved and checked if it can happen at all
-
-        broadcast {
-            append("$COLOR_YELLOW melted to")
-            append("$COLOR_RED Death")
-        }
-    }
-
-    override fun byPoison() {
-        // FIX ME: This death reason should be improved and checked if it can happen at all
-
-        broadcast {
-            append("$COLOR_YELLOW poisoned to")
-            append("$COLOR_RED Death")
-        }
-    }
-
-    override fun byStarvation() {
-        broadcast {
-            append("$COLOR_YELLOW starved to")
-            append("$COLOR_RED Death")
-        }
-    }
-
-    override fun bySuffocation() {
-        broadcast {
-            append("$COLOR_YELLOW suffocated in")
-            append("$COLOR_GRAY Wall")
-        }
-    }
-
-    override fun bySuicide() {
-        broadcast {
-            append("$COLOR_YELLOW died")
-        }
-    }
-
-    override fun byThorns() {
-        broadcast {
-            append("$COLOR_YELLOW was finished by")
-            append("$COLOR_GREEN Cactus")
-        }
-    }
-
-    override fun byVoid() {
-        broadcast {
-            append("$COLOR_YELLOW fell into the void")
-        }
-    }
-
-    override fun byWither() {
-        broadcast {
-            append("$COLOR_YELLOW was killed by")
-            append("$COLOR_PURPLE Wither")
-        }
-    }
-
-    override fun unknown(event: EntityDamageEvent) {
-        broadcast {
-            append("$COLOR_YELLOW died")
-        }
-    }
-
-    protected inline fun broadcast(block: StringBuilder.() -> Unit) {
-        val builder = StringBuilder().apply {
-            append(name)
-            block(this)
-
-            val dimension = Dimension.get(gameObject.entity.world)
-
-            if (dimension != Dimension.WORLD)
-                append("$COLOR_YELLOW in ${dimension.displayName}")
-        }
-
-        val message = builder.toString()
-
-        // Bukkit.broadcastMessage() is expensive since it broadcast with permission
-        Game.players.values.forEach {
-            it.entity.run {
-                sendMessage(message)
-            }
-        }
-        Bukkit.getConsoleSender().sendMessage(message)
-    }
-
-    abstract override fun byPlayer(killer: GamePlayer, event: EntityDamageByEntityEvent)
 }
