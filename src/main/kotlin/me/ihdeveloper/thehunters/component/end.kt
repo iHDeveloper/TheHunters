6e7b7381bfbf1a50c98aca9af9ff41afc6de90e2
iHDeveloper <20463031+iHDeveloper@users.noreply.github.com>
2020-06-26 10:05:06 +0300

    Create component for the side that won in the end of the game

---
+++ src/main/kotlin/me/ihdeveloper/thehunters/component/end.kt
@@ -0,0 +1,61 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 iHDeveloper ( Hamza Aljuaid )
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package me.ihdeveloper.thehunters.component
+
+import me.ihdeveloper.thehunters.GameComponentOf
+import me.ihdeveloper.thehunters.GamePlayer
+import me.ihdeveloper.thehunters.util.COLOR_BOLD
+import me.ihdeveloper.thehunters.util.COLOR_GREEN
+import me.ihdeveloper.thehunters.util.COLOR_YELLOW
+
+const val TYPE_END_VICTORY: Short = 901
+
+class VictoryComponent (
+        val target: Boolean,
+        override val gameObject: GamePlayer
+) : GameComponentOf<GamePlayer>() {
+
+    override val type = TYPE_END_VICTORY
+
+    override fun onInit(gameObject: GamePlayer) {
+        gameObject.get<TitleComponent>(TYPE_TITLE).run {
+            reset()
+            title("$COLOR_GREEN${COLOR_BOLD}You won!")
+            subtitle(StringBuilder().apply {
+                append("$COLOR_YELLOW")
+                append("$COLOR_BOLD")
+                if (target)
+                    append("You proved that you can't be hunted!")
+                else
+                    append("Good hunt!")
+            }.toString())
+            time(10, 60, 10)
+        }
+    }
+
+    override fun onDestroy(gameObject: GamePlayer) {}
+
+}
